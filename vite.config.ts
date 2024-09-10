ad51541156f4d201025265f065430b9140081390
Joao Dantas <joaostadantas@gmail.com>
2024-09-10 20:27:08 -0300

    feat: setup vitest and react testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+/// <reference types="vitest/config" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/vitest.setup.ts'],
+  },
   plugins: [react()],
 })
+
+
